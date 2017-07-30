006d346a5682cc7cb4b7c119c5ee1f2578c4d30f
John Eikenberry <jae@zhar.net>
2017-07-30 01:02:49 -0700

    add go package doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package udev-notify is a tool to watch for device Udev events, matching on a
+// property and running a configured command. Designed to run as part of a user
+// session on a Linux system.
+//
+// See the README.md file in the repository for more.
+package main
